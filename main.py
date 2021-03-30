3e2780b7633713a429d36ed01121c015ade2966c
AmrMualla <muallaamr@gmail.com>
2021-03-30 17:48:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"Amr Mualla, FizzBuzz Solution, 14 Seconds."
+
+for x in range(0, 101):
+  if x % 3 == 0 and x % 5 == 0:
+    print("FizzBuzz")
+  elif x % 3 == 0:
+    print("Fizz")
+  elif x % 5 == 0:
+    print("Buzz")
+  else:
+    print(x)
